c07398f4cd6a4a28e578ada537d8c5ca2dee299c
Sen Chao <senchao0930@gmail.com>
2022-07-27 13:52:47 +0800

    Sen second commit

--- tests/test_demo1.py
+++ tests/test_demo1.py
@@ -5,25 +5,13 @@ from pydantic import BaseModel
 from typing import Dict, Any
 import pandas as pd
 
-# class a(BaseModel):
-#     a : int
-#     b : Dict[str, int]
-
-# class Model(BaseModel):
-#     model_id: str = None
-#     model_name: str = None
-#     model_data: Any = None
-#     experiment_id: str = None
-#     experiment_name: str = None
-#     attributes: Dict[str, Any] = None
-
 def test_demo1():
    print("---From test demo1---")
 
    client = Client(
-      auth_key="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiNjJkZjVmMGRkOTJkNmExYzMxY2QwNTE0IiwiaXNzIjoiZ3AtYmUiLCJzdWIiOiJhcGlrZXkiLCJhdWQiOiJjbGllbnQiLCJpYXQiOjE2NTg4MDYwMjkuMjEyMDg2fQ.C-fo1_69ubOBtDpuZ3izBX9v7jgUWjersCOKmmS9bSM", 
-      project_id="62df5f26c64f9b760444d911", 
-      host="192.168.2.18")
+      auth_key="auth_key", 
+      project_id="project_id", 
+      host="host")
 
    current_path = os.path.dirname(os.path.abspath(__file__))
    train_file_path = os.path.join(current_path, "train.csv")
