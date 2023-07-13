5e738afdd57e0ae30a21afddacc5ed9ce3c5f999
jimliu <jimliu@mobagel.com>
2023-07-13 04:27:48 -0400

    fix: typo and re-naming

--- tests/integration/test_ts_e2e.py
+++ tests/integration/test_ts_e2e.py
@@ -9,8 +9,8 @@ from decanter_ai_sdk.enums.time_units import TimeUnit
 def test_ts(client):
     print("---From test ts---")
     train_file_path = "data/ts_train.csv"
-    train_file_df = pd.read_csv(open(train_file_path, "rb"))
-    train_id = client.upload(train_file_df, "train_file")
+    train_df = pd.read_csv(open(train_file_path, "rb"))
+    train_id = client.upload(train_df, "train_file")
     assert train_id is not None
 
     test_file_path = "data/ts_test.csv"
@@ -41,14 +41,14 @@ def test_ts(client):
         keep_columns=[], non_negative=False, test_table_id=test_id, model=best_model
     )
     
-    first_mode_id = client.get_model_list(experiment.id)[0]['_id']
-    pred_proba = client.batch_predict(
-        pred_df=train_file_df,
+    first_model_id = client.get_model_list(experiment.id)[0]['_id']
+    predictions = client.batch_predict(
+        pred_df=train_df,
         experiment_id=experiment.id,
-        model_id=first_mode_id)
+        model_id=first_model_id)
 
     assert predict.attributes["status"] == "done"
     assert isinstance(predict.get_predict_df(), pd.DataFrame)
-    assert isinstance(pred_proba, pd.Series)
+    assert isinstance(predictions, pd.Series)
     assert predict.attributes["model_id"] == best_model.model_id
     assert predict.attributes["table_id"] == test_id
