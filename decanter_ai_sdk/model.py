2303f6a1604c34e25d8ec22b31190c45307c448e
reneewu <reneewu@mobagel.com>
2023-08-23 18:17:06 +0800

    Integrate the "train_fusion_model" parameter into the "train_ts" function

--- decanter_ai_sdk/model.py
+++ decanter_ai_sdk/model.py
@@ -1,11 +1,12 @@
-from typing import Dict
+from typing import Dict, Union
+
 from pydantic import BaseModel
 
 
 class Model(BaseModel):
     model_id: str
     model_name: str
-    metrics_score: Dict[str, float]
+    metrics_score: Dict[str, Union[float, None]]
     experiment_id: str
     experiment_name: str
     attributes: Dict
